e4f4fc035bc608243f710593376c8dd4eefa9501
Sakura523-A <imotoshori@gmail.com>
2025-12-26 21:44:17 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        //変数に値を代入する
+        $vegetables = ['name'=>'onion',
+        'price'=>200,
+        'weight'=>160
+        ];
+        //
+        print_r($vegetables);
+        ?>
+    </p>
