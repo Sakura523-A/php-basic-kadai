62541d28fd82ad026bc1d8b0ad37427b136e95ae
Sakura523-A <imotoshori@gmail.com>
2026-01-04 20:03:57 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+<body>
+    <p>
+<?php
+        class Food {
+        public $name;
+        public $price;
+
+        function __construct(string $name, int $price){
+            $this->name = $name;
+            $this->price = $price;
+        }
+        public function show_price(){
+            echo $this->price;
+        }
+    }
+
+    class Animal {
+        public $name;
+        public $height;
+        public $weight;
+
+        function __construct(string $name, int $height, int $weight){
+            $this->name = $name;
+            $this->height = $height;
+            $this->weight = $weight;
+        }
+        public function show_height(){
+            echo $this->height;
+        }
+    }
+    $baked_sweet_potato = new Food("焼き芋", 150);
+    print_r($baked_sweet_potato);
+    echo "<br>";
+    $dog = new Animal("ポチ", 60, 15);
+    print_r($dog);
+    echo "<br>";
+    print_r($dog->show_height());
+    echo "<br>";
+    print_r($baked_sweet_potato->show_price());
+?>
+    </p>
+</body>
+</html>
